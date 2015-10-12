142d06209210439edeabe3dce38bebff520e2856
PZdunczyk <Patryk.Zdunczyk@stonybrook.edu>
2015-10-12 14:31:49 -0400

    3rd commit

--- src/fortunewheel/Fortunewheel.java
+++ src/fortunewheel/Fortunewheel.java
@@ -1,12 +1,16 @@
 
 package fortunewheel;
-
+import java.util.Scanner;
 
 public class Fortunewheel {
 
     
     public static void main(String[] args) {
-       
+       Scanner in = new Scanner(System.in);
+       String dificulty;
+       System.out.println("Select dificulty: ");
+       dificulty = in.nextLine();
+               
     }
     
 }
